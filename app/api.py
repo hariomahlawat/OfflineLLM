3dda18257ab401dd301a8452e53b0222b206e358
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-07-12 05:58:36 +0530

    models not loading in UI dropdown.

--- app/api.py
+++ app/api.py
@@ -49,7 +49,7 @@ else:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
